95b30ac1af495ec4b25e9acfc015f4e95766686b
Arnab Chatterjee <arnabc1701@gmail.com>
2021-09-20 23:28:10 +0530

    first commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+
+export default function Navbar() {
+    return (
+        <div>
+            <Button className="font-jakarta font-bold focus:ring text-6xl" variant="contained">Quick start</Button>
+        </div>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
+import '../styles/global.css'
+import "@fontsource/plus-jakarta-sans";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
